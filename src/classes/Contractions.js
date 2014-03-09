00c83bbd6d0f5df604858863a6229037d5d44b77
Brannon Dorsey <brannon@brannondorsey.com>
2014-03-09 12:04:23 -0500

    Improving Contraction class. Currently not working because in the middle of changes.

--- src/classes/Contractions.js
+++ src/classes/Contractions.js
@@ -29,14 +29,71 @@ function Contractions(){
 	this.contractions = contractions;
 }
 
+/*
+	options: {
+		misspelled: true,
+		tense: "present"
+	}
+ */
 Contractions.prototype.expand = function(string, options){
+	var words = splitSentence(string);
+	var newSentence = [];
+	var self = this;
+	_.each(words, function(word){
+		newSentence.push(self._translate("expansion", string, options));
+	});
+	// come back here
+}
 
+Contractions.prototype.contract = function(string, options){
+	return this._translate("contraction", string, options);
+}
+
+Contractions.prototype.splitSentence = function(string){
+	var string = string.replace(/[^\w\s]|_/g, function ($1) { return ' ' + $1 + ' ';})
+	string = string.replace(/[ ]+/g, ' ').split(' ');
+	var nullChar = _.indexOf(string, '');
+	if (nullChar != -1) {
+		string.splice(nullChar, 1);
+	}
+	return string;
+}
+
+Contractions.prototype._translate = function(type, string, options){
+
+	var matchUsing = (type == "expansion")? "contraction" : "expansion"; 
 	// be sure to check if is capitalized and return using same style
+	var isFirstCap = (string.charAt(0) == string.charAt(0).toUpperCase());
+	var isAllCaps = (string == string.toUpperCase());
+	var string = string.toLowerCase();
 	
-	var match = _.findWhere(this.contractions, { contraction: string });
+	var match;
+
+	_.each(this.contractions, function(obj){
+		if (_.isArray(obj[matchUsing])) {
+			_.each(obj[matchUsing], function(word){
+				
+				if (word.toLowerCase() == string) {
+					match = obj;
+					return;
+				}
+			});
+		} else {
+			if (obj[matchUsing].toLowerCase() == string) {
+				match = obj;
+				return;
+			}
+		}
+	});
 	
 	if (_.isUndefined(match)) {
+		
 		_.each(this.contractions, function(obj){
+			
+			var misspellings = _.map(obj.misspellings, function(word){
+				return word.toLowerCase();
+			});
+
 			var index = _.indexOf(obj.misspellings, string);
 			if (index != -1) {
 				match = obj;
@@ -44,16 +101,25 @@ Contractions.prototype.expand = function(string, options){
 			} 
 		});
 	}
+	
+	var returnVal;
 
 	if (!_.isUndefined(match)) {
-		return match.expansion[0];
+		if (_.isArray(match[type])) {
+			returnVal = match[type][0];
+		} else {
+			returnVal = match[type];
+		}
 	} else {
-		return string;
+		returnVal = string;
 	}
-}
 
-Contractions.prototype.contract = function(){
+	if (isFirstCap){
+		returnVal = returnVal.charAt(0).toUpperCase() + returnVal.substring(1);
+	}
+	if (isAllCaps) returnVal = returnVal.toUpperCase();
 
+	return returnVal;
 }
 
 module.exports = Contractions;
--- src/contraction_test.js
+++ src/contraction_test.js
@@ -2,11 +2,16 @@ var Contractions = require('./classes/Contractions.js'),
 _ = require('underscore');
 var contractions = new Contractions();
 
-var sentence = "thatl be wer";
-var temp = [];
+var sentence = "CNT make it to the show, sorry were running late.";
+var cont = [];
+var expa = [];
 
 _.each(sentence.split(' '), function(word){
-	temp.push(contractions.expand(word));
+	expa.push(contractions.expand(word));
+	cont.push(contractions.contract(word));
 });
 
-console.log(temp.join(' '));
+console.log(contractions.splitSentence("test"));
+console.log("Original: " + sentence);
+console.log("Contraction: " + cont.join(' '));
+console.log("Expansion: " + expa.join(' '));
