dcb0c0ea611317179a02b77ae20d1d2b3d1117a0
Brannon Dorsey <brannon@brannondorsey.com>
2014-04-08 17:35:26 -0500

    Generating ngrams for being

--- src/classes/BeingObjectGenerator.js
+++ src/classes/BeingObjectGenerator.js
@@ -13,6 +13,7 @@ var _patternHelper = new PatternHelper();
 var _normalizer = new Normalizer();
 var _smsSentiment = new SMSSentiment();
 var _tokenizer = new _natural.WordTokenizer();
+var _personalCorpus;
 
 function BeingObjectGenerator() {
 	this.infoFile = '/info.json';
@@ -43,16 +44,90 @@ BeingObjectGenerator.prototype.getBeingObject = function(corpusPath, callback) {
 		beingObj.location = info.location;
 		beingObj.phoneNumber = info.phoneNumber;
 		beingObj.interests = info.interests;
-		beingObj.model = {};
+		
 
-		var personalCorpus = new PersonalCorpus(corpusPath + self.corpusFile, function(corpus){
+		_personalCorpus = new PersonalCorpus(corpusPath + self.corpusFile, function(corpus){
 
+			beingObj.model = {};
+			beingObj.model.ngrams = _getNgrams();
+			console.log(JSON.stringify(beingObj.model.ngrams.bigram));
 			callback(beingObj);
 		});
 	});
 }
 
 BeingObjectGenerator.prototype.regenerateBeingModel = function(beingObj) {
+
+}
+
+function _getNgrams() {
+
+	var self = this;
+	var messagesFromBeing = _.filter(_personalCorpus.messages, function(message){
+		return message.from == "Me";
+	});
+
+	var messageStartToken = "<MESSAGE>";
+	var messageEndToken = "</MESSAGE>";
+	var words = _getWords();
+
+	return {
+		words: words,
+		bigram: _getBigram()
+	};
+
+	function _getWords() {
+		
+		var words = [];
+
+		_.each(messagesFromBeing, function(message){
+			_.each(message.sentences, function(sentence){
+				_.each(sentence.words, function(word){
+					if (_.indexOf(words, word) == -1) {
+						words.push(word);
+					}
+				});
+			});
+		});
+		
+		words = _.sortBy(words, function (word) {return word}).reverse();
+		words.unshift(messageEndToken);
+		words.unshift(messageStartToken);
+		return words;
+	}
+
+	function _getBigram() {
+
+		// initializing a javascript array with
+		// values is a pain in the ass...
+		var bigram = new Array(words.length);
+		for (var i = 0; i < bigram.length; i++) {
+			bigram[i] = [];
+			for (var j = 0; j < bigram.length; j++) {
+				bigram[i][j] = 0;
+			}	
+		}
+
+		var previousWord = "";
+
+		_.each(messagesFromBeing, function(message){
+			previousWord = messageStartToken;
+			_.each(message.sentences, function(sentence){
+				_.each(sentence.words, function(word){
+					var previousIndex = _.indexOf(words, previousWord);
+					var thisIndex = _.indexOf(words, word);
+					bigram[previousIndex][thisIndex]++;
+					previousWord = word;
+				});
+			});
+
+			var previousIndex = _.indexOf(words, previousWord);
+			var thisIndex = _.indexOf(words, messageEndToken);
+			bigram[previousIndex][thisIndex]++;
+		});
+		
+		return bigram;
+	}
 
 }
 
--- src/tests/being_generator_test.js
+++ src/tests/being_generator_test.js
@@ -2,5 +2,5 @@ var BeingObjectGenerator = require('../classes/BeingObjectGenerator');
 
 var beingObjGenerator = new BeingObjectGenerator();
 beingObjGenerator.getBeingObject('data/corpuses/cory_feder', function(beingObj){
-	console.log(beingObj);
+	// console.log(beingObj);
 });
